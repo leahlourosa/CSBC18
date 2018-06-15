95dd291502c2b3aa0da0035a6cd4b4ff59ab4a2a
leahlourosa <leahlourosa@gmail.com>
2018-06-15 11:03:07 -0700

    complete bigger_better_favorite_number

---
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -0,0 +1,4 @@
+puts 'what\'s your favorite number dude?:'
+favorite = gets.chomp
+better = favorite.to_i + 1
+puts 'Bigger\'s alawys better, right? so you should probably get rid of that trash number and like ' + better.to_s + " instead"
