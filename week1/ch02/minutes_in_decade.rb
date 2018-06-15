b522e026f9deceafffa39e290df259011548338a
leahlourosa <leahlourosa@gmail.com>
2018-06-15 11:28:00 -0700

    complete minutes_in_decade

--- week1/ch02/minutes_in_decade.rb
+++ week1/ch02/minutes_in_decade.rb
@@ -2,4 +2,4 @@ puts 'How many leap years in the last decade?'
   name = gets.chomp
   leap = name.to_i * 1440
   final = 5256000 + leap
-puts 'there are ' + final.to_s + 'minutes in this decade'
+puts 'there are ' + final.to_s + ' minutes in this decade'
