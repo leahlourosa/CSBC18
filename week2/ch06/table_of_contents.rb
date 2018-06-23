1ba4ba108560a40c19f7927dbc0328520c4ae3a5
leahlourosa <leahlourosa@gmail.com>
2018-06-22 22:55:20 -0700

    complete table_of_contents

---
+++ week2/ch06/table_of_contents.rb
@@ -0,0 +1,5 @@
+width = 25
+puts 'Table of contents'.center(50)
+puts 'Chapter 1: Getting Started'.ljust(width) + 'Page 1'.rjust(24)
+puts 'Chapter 2: Numbers'.ljust(width) + 'Page 9'.rjust(width)
+puts 'Chapter 3: Letters'.ljust(width) + 'Page 13'.rjust(width)
