280049f701a0e77d6d35ad26dfce2d226dbce369
leahlourosa <leahlourosa@gmail.com>
2018-07-06 23:32:09 -0700

    complete digit_sum.rb

--- week4/recursion_practice/digit_sum.rb
+++ week4/recursion_practice/digit_sum.rb
@@ -1,9 +1,13 @@
-def digit_sum(num, ___)
-  if ______________________
-    ______________________
+def digit_sum(num)
+  if num.length == 1
+    return num.to_i
   else
-    ______________________
+    return  num.slice!(0).to_i +  digit_sum(num)
   end
 end
 
-puts digit_sum __, __ 
+
+
+
+
+puts digit_sum('1234')
