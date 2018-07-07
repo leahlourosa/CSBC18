b9dc3ff6da3a9f68f81936d318392db39572eec8
leahlourosa <leahlourosa@gmail.com>
2018-07-06 23:42:01 -0700

    complete array_min.rb

--- week4/recursion_practice/array_min.rb
+++ week4/recursion_practice/array_min.rb
@@ -1,9 +1,19 @@
-def array_min(array, len)
-  if ______________________
-    array[0]
+def array_min(array)
+  if array.length == 1
+    return array[0]
   else
-    ______________________
+    first = array.pop
+    newnum = array[0]
+     if first > newnum
+       first = newnum
+
+    end
+array.push(first)
+array.delete_at(0)
+
   end
+array_min(array)
+
 end
 
-puts array_min __, __ 
+puts array_min([2, 3, 1])
