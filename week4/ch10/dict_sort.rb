a1d9ae0b686801713fb2dc76d491052c905529da
leahlourosa <leahlourosa@gmail.com>
2018-07-06 23:26:45 -0700

    complete dict_sort.rb

--- week4/ch10/dict_sort.rb
+++ week4/ch10/dict_sort.rb
@@ -1,9 +1,23 @@
-def dict_sort(some_array) # This "wraps" recursive_sort.
+def sorting(some_array) # This "wraps" recursive_sort.
   recursive_sort some_array, []
 end
 
-def recursive_dict_sort(unsorted_array, sorted_array)
-  # Your fabulous code goes here.
+def recursive_sort(unsorted_array, sorted_array)
+  if unsorted_array.length <= 0
+return sorted_array
 end
-
-dict_sort [__, __, ...]
+smallest = unsorted_array[0]
+unsorted_array.delete_at(0)
+working_array = []
+unsorted_array.each do |testing|
+  if testing.downcase < smallest.downcase
+            working_array.push smallest
+            smallest = testing
+else
+            working_array.push testing
+end
+end
+sorted_array.push smallest
+  recursive_sort working_array, sorted_array
+end
+puts sorting ['can','feel','Singing','like','a','can']
